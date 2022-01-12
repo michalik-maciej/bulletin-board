abfa922ce7571442fbd77fc8ca71b349ffe464ee
michalik-maciej <halniak24@gmail.com>
2022-01-12 21:54:07 +0100

    Need fix customAlert

--- src/components/common/CustomAlert/CustomAlert.test.js
+++ src/components/common/CustomAlert/CustomAlert.test.js
@@ -3,6 +3,12 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 describe('Component CustomAlert', () => {
+  /*   const mockedUsedLocation = jest.fn()
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useLocation: () => mockedUsedLocation,
+  })) */
+
   it('should render without crashing', () => {
     const component = shallow(<CustomAlertComponent />)
     expect(component).toBeTruthy()
